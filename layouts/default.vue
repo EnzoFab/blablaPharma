9d6ee8b78be5e50dc6cf5d5bcac53077ce6e1421
Enzo Fabre <fabre.enzoj@gmail.com>
2019-07-11 00:39:54 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app light>
+    <v-toolbar
+      :scroll-threshold="150"
+      app
+      fixed
+      flat
+      scroll-off-screen
+      color="grey lighten-5"
+    >
+      <v-toolbar-items>
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <app-footer class="mt-4" />
+  </v-app>
+</template>
+
+<script>
+import AppFooter from "~/components/layout/AppFooter";
+export default {
+  components: {
+    AppFooter
+  },
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+const colors = require("vuetify/es5/util/colors").default;
+
+module.exports = {
+  mode: "universal",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://db.onlinewebfonts.com/c/9a7e574c3eda05d71d03345d3f9e268e?family=FuturaLTW01-LightOblique"
+      }
+    ]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: ["@assets/scss/main.scss"],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    "@nuxtjs/vuetify",
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa"
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    theme: {
+      primary: colors.blue.darken2,
+      accent: colors.grey.darken3,
+      secondary: colors.amber.darken3,
+      info: colors.teal.lighten1,
+      warning: colors.amber.base,
+      error: colors.deepOrange.accent4,
+      success: colors.green.accent3
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
