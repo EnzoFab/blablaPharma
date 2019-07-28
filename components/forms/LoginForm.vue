b5fff298e30b0bf9f901d55bc1f5118c32cab726
Enzo Fabre <fabre.enzoj@gmail.com>
2019-07-29 00:40:22 +0200

    login

--- components/forms/LoginForm.vue
+++ components/forms/LoginForm.vue
@@ -1,8 +1,10 @@
 <template>
   <v-dialog :value="value" fullscreen full-width transition persistent>
-    <v-card>
+    <v-card style="background-color: rgba(255, 255, 255, 0.97);">
       <v-card-text>
-        <v-icon x-large @click="$emit('input', false)">close</v-icon>
+        <v-flex offset-sm10 sm2 offset-xs5 xs2>
+          <v-icon x-large @click="$emit('input', false)">close</v-icon>
+        </v-flex>
       </v-card-text>
       <v-card-title primary>
         <h1 class="title-main title-main-rail content-center text--baseColor">
@@ -11,27 +13,39 @@
       </v-card-title>
       <div class=" text--section content-center">
         <span class="text--baseColor">Pas encore de compte ?</span>
-        <nuxt-link
-          tag="a"
-          to="/sign-in"
+        <a
           class="text--section text-no-decoration blue-grey--text text--lighten-3"
-          >s'inscrire</nuxt-link
+          @click="gotoSignIn"
+          >s'inscrire</a
         >
       </div>
       <v-container fluid>
         <v-layout row wrap>
-          <v-flex xs4 offset-xs4>
-            <v-form class="px-5 mt-4">
+          <v-flex sm4 offset-sm4 xs9 offset-xs1>
+            <v-form class="px-5 mt-4" ref="login">
               <v-text-field
+                v-model="mail"
                 label="Email"
                 color="blue-grey lighten-1"
+                type="mail"
               ></v-text-field>
               <v-text-field
+                v-model="password"
                 label="Mot de passe"
                 color="blue-grey lighten-1"
                 class="mt-4"
                 type="password"
               ></v-text-field>
+              <v-btn
+                large
+                block
+                depressed
+                color="blue-grey lighten-1"
+                dark
+                class="mt-4"
+                @click="connexion"
+                >Connexion</v-btn
+              >
             </v-form>
           </v-flex>
         </v-layout>
@@ -48,8 +62,20 @@ export default {
   },
   data() {
     return {
-      visible: this.value
+      email: "",
+      password: ""
     };
+  },
+  methods: {
+    gotoSignIn() {
+      this.$router.push({ path: "/sign-in" });
+      this.$emit("input", false);
+    },
+    connexion() {
+      if (this.$refs.login.validate()) {
+      }
+      // todo call api login
+    }
   }
 };
 </script>
